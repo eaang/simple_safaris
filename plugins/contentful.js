6cd75caea26245f8691fee7273eb04f999a16b1c
Evan Ang <alethiologist@gmail.com>
2020-09-21 00:03:42 +0800

    missed the plugin one

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const contentful = require('contentful')
 
 module.exports = contentful.createClient({
-  space: process.env.NUXT_ENV_CTF_SPACE_ID,
-  accessToken: process.env.NUXT_ENV_CTF_CD_ACCESS_TOKEN,
+  space: process.env.CTF_SPACE_ID,
+  accessToken: process.env.CTF_CD_ACCESS_TOKEN,
 })
