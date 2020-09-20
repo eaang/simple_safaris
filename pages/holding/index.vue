893049efba511dc1d5b9ffe2cb57a7796488cfa1
Evan Ang <alethiologist@gmail.com>
2020-09-20 22:58:11 +0800

    rebuilding from scratch

---
+++ pages/holding/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="relative landing-page">
+    <TheTransparentHeader
+      :continents="continents"
+      :trip-ideas="tripIdeas"
+      class="absolute w-screen z-50"
+    />
+    <TheSidenav class="absolute inset-x-0 top-0" />
+    <TheFullheightHero class="z-40" />
+  </div>
+</template>
+
+<script>
+import TheTransparentHeader from '@/components/Navigation/TheTransparentHeader.vue'
+import TheSidenav from '@/components/Navigation/TheSidenav.vue'
+import TheFullheightHero from '@/components/Hero/TheFullheightHero.vue'
+
+export default {
+  components: {
+    TheTransparentHeader,
+    TheSidenav,
+    TheFullheightHero,
+  },
+  async asyncData({ store, params }) {
+    await store.dispatch('continents/getContinents')
+    await store.dispatch('tripIdeas/getTripIdeas', params.slug)
+    return {
+      continents: store.getters['continents/continents'],
+      tripIdeas: store.getters['tripIdeas/tripIdeas'],
+    }
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,12 @@
 <template>
-  <div class="relative landing-page">
-    <TheTransparentHeader
-      :continents="continents"
-      :trip-ideas="tripIdeas"
-      class="absolute w-screen z-50"
-    />
-    <TheSidenav class="absolute inset-x-0 top-0" />
-    <TheFullheightHero class="z-40" />
+  <div class="landing-page">
+    {{ continents }}
+    {{ tripIdeas }}
   </div>
 </template>
 
 <script>
-import TheTransparentHeader from '@/components/Navigation/TheTransparentHeader.vue'
-import TheSidenav from '@/components/Navigation/TheSidenav.vue'
-import TheFullheightHero from '@/components/Hero/TheFullheightHero.vue'
-
 export default {
-  components: {
-    TheTransparentHeader,
-    TheSidenav,
-    TheFullheightHero,
-  },
   async asyncData({ store, params }) {
     await store.dispatch('continents/getContinents')
     await store.dispatch('tripIdeas/getTripIdeas', params.slug)
