b648ade55fc21ec11f1a8259bce3d9adc759bb99
Evan Ang <alethiologist@gmail.com>
2020-09-20 23:12:47 +0800

    changed to loading the fonts in nuxt config head

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,15 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap',
+      },
+      { rel: 'stylesheet', href: 'https://use.typekit.net/oqp4bki.css' },
+    ],
   },
   router: {
     base: '/',
