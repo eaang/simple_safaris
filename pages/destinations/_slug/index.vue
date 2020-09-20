eba9c7661d036824a6c8154c2b2f79e177d58768
Evan Ang <alethiologist@gmail.com>
2020-09-20 18:38:08 +0800

    allowed for empty destinations

--- pages/destinations/_slug/index.vue
+++ pages/destinations/_slug/index.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    This is the page for an individual country (shows all trip ideas for that
-    one?)
+    This is the page for a single destination.
+    <div>{{ destinationName }}</div>
+    {{ destinationDescription }}
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async fetch({ store, params }) {
+    await store.dispatch('destination/getDestinationBySlug', params.slug)
+  },
+  computed: {
+    currentDestination() {
+      return this.$store.getters['destination/destination']
+    },
+    destinationName() {
+      return this.$store.getters['destination/destinationName']
+    },
+    destinationDescription() {
+      return this.$store.getters['destination/destinationDescription']
+    },
+    isLoading() {
+      return this.$store.getters['destination/loadingStatus']
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
