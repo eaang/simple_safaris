d9cd172958e56f46fe86dd07472393ea8b07aca5
Evan Ang <alethiologist@gmail.com>
2020-09-20 23:07:47 +0800

    seeing what the tailwind issue is

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="landing-page">
-    <div v-for="(continent, index) in continents" :key="index">
-      {{ continent.fields.name }}
-      <div v-for="(place, ind) in continent.fields.destinations" :key="ind">
-        <a :href="'/destinations/' + place.fields.slug">{{
-          place.fields.name
-        }}</a>
+    <div class="container mx-auto">
+      <div v-for="(continent, index) in continents" :key="index">
+        {{ continent.fields.name }}
+        <div v-for="(place, ind) in continent.fields.destinations" :key="ind">
+          <a :href="'/destinations/' + place.fields.slug">{{
+            place.fields.name
+          }}</a>
+        </div>
       </div>
-    </div>
-    <div v-for="(trip, id) in tripIdeas" :key="id">
-      <a :href="'/trips/' + trip.fields.slug">{{ trip.fields.name }}</a>
+      <div v-for="(trip, id) in tripIdeas" :key="id">
+        <a :href="'/trips/' + trip.fields.slug">{{ trip.fields.name }}</a>
+      </div>
     </div>
   </div>
 </template>
