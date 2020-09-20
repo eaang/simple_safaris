7b28ce2f7a66598a5ce90162f41ec085f6e742ea
Evan Ang <alethiologist@gmail.com>
2020-09-20 21:23:41 +0800

    minor tweaks

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
     exposeConfig: true,
   },
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/vueAgile.js', ssr: false }],
+  plugins: ['~/plugins/vueAgile.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
