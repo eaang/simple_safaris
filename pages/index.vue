1c05a302c2c7b6661c3e319f4652019b4c14630c
Evan Ang <alethiologist@gmail.com>
2020-09-20 23:03:46 +0800

    basic links up

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,13 @@
     <div v-for="(continent, index) in continents" :key="index">
       {{ continent.fields.name }}
       <div v-for="(place, ind) in continent.fields.destinations" :key="ind">
-        {{ place.fields.name }}
+        <a :href="'/destinations/' + place.fields.slug">{{
+          place.fields.name
+        }}</a>
       </div>
     </div>
     <div v-for="(trip, id) in tripIdeas" :key="id">
-      {{ trip.fields.name }}
+      <a :href="'/trips/' + trip.fields.slug">{{ trip.fields.name }}</a>
     </div>
   </div>
 </template>
