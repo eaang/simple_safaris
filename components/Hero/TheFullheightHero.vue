aa833efe8496bb9f2b75d43d55f8dea5521c31fb
Evan Ang <alethiologist@gmail.com>
2020-09-20 19:36:54 +0800

    removed weird client side thing

--- components/Hero/TheFullheightHero.vue
+++ components/Hero/TheFullheightHero.vue
@@ -1,63 +1,59 @@
 <template>
   <div>
-    <client-only>
-      <div class="relative h-screen w-full">
-        <agile ref="carousel" :options="carouselOptions">
-          <div
-            v-for="image in images"
-            :key="image.pathShort"
-            class="slide block h-screen w-full z-0 bg-center bg-cover"
-            :style="{
-              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0)), url(${image.pathLong})`,
-            }" />
+    <div class="relative h-screen w-full">
+      <agile ref="carousel" :options="carouselOptions">
+        <div
+          v-for="image in images"
+          :key="image.pathShort"
+          class="slide block h-screen w-full z-0 bg-center bg-cover"
+          :style="{
+            backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0)), url(${image.pathLong})`,
+          }" />
 
-          <template slot="prevButton"
-            ><div
-              class="absolute inset-y-0 left-0 flex items-center justify-around px-12 text-4xl"
-              @click="counter--"
+        <template slot="prevButton"
+          ><div
+            class="absolute inset-y-0 left-0 flex items-center justify-around px-12 text-4xl"
+            @click="counter--"
+          >
+            <div
+              class="rounded-full border-2 border-white p-8 text-white hover:bg-white hover:text-brown"
             >
-              <div
-                class="rounded-full border-2 border-white p-8 text-white hover:bg-white hover:text-brown"
-              >
-                <div class="h-6 w-6 flex items-center justify-around">
-                  <fa-icon icon="arrow-left" />
-                </div>
+              <div class="h-6 w-6 flex items-center justify-around">
+                <fa-icon icon="arrow-left" />
               </div>
             </div>
-          </template>
-          <template slot="nextButton"
-            ><div
-              class="absolute inset-y-0 right-0 flex items-center justify-around px-12 text-4xl"
-              @click="counter++"
+          </div>
+        </template>
+        <template slot="nextButton"
+          ><div
+            class="absolute inset-y-0 right-0 flex items-center justify-around px-12 text-4xl"
+            @click="counter++"
+          >
+            <div
+              class="rounded-full border-2 border-white p-8 text-white hover:bg-white hover:text-brown"
             >
-              <div
-                class="rounded-full border-2 border-white p-8 text-white hover:bg-white hover:text-brown"
-              >
-                <div class="h-6 w-6 flex items-center justify-around">
-                  <fa-icon icon="arrow-right" />
-                </div>
+              <div class="h-6 w-6 flex items-center justify-around">
+                <fa-icon icon="arrow-right" />
               </div>
-            </div> </template
-        ></agile>
+            </div>
+          </div> </template
+      ></agile>
+    </div>
+    <!-- Custom dots -->
+    <ul class="flex justify-around absolute inset-x-0 bottom-0 w-screen py-12">
+      <div class="flex">
+        <li
+          v-for="(image, i) in images"
+          :key="i"
+          class="border-2 border-white rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-white"
+          :class="{ 'bg-white': i === trueCounter }"
+          @click="
+            counter = i
+            $refs.carousel.goTo(trueCounter)
+          "
+        ></li>
       </div>
-      <!-- Custom dots -->
-      <ul
-        class="flex justify-around absolute inset-x-0 bottom-0 w-screen py-12"
-      >
-        <div class="flex">
-          <li
-            v-for="(image, i) in images"
-            :key="i"
-            class="border-2 border-white rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-white"
-            :class="{ 'bg-white': i === trueCounter }"
-            @click="
-              counter = i
-              $refs.carousel.goTo(trueCounter)
-            "
-          ></li>
-        </div>
-      </ul>
-    </client-only>
+    </ul>
   </div>
 </template>
 
