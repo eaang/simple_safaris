0631d9ab58c080835ae7dd64ed5e91b57487511b
Evan Ang <alethiologist@gmail.com>
2020-09-20 23:29:37 +0800

    simple button test

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,25 @@
   <div class="landing-page">
     <TheTransparentHeader />
     <div class="container mx-auto">
-      <div v-for="(continent, index) in continents" :key="index">
+      <div v-for="continent in continents" :key="continent.fields.name">
         {{ continent.fields.name }}
-        <div v-for="(place, ind) in continent.fields.destinations" :key="ind">
+        <div
+          v-for="place in continent.fields.destinations"
+          :key="place.fields.name"
+        >
           <a :href="'/destinations/' + place.fields.slug">{{
             place.fields.name
           }}</a>
         </div>
       </div>
-      <div v-for="(trip, id) in tripIdeas" :key="id">
+      <div v-for="trip in tripIdeas" :key="trip.fields.name">
         <a :href="'/trips/' + trip.fields.slug">{{ trip.fields.name }}</a>
+      </div>
+      <div
+        class="rounded-lg px-4 py-2 bg-teal-500 text-white w-40 text-center cursor-pointer"
+        @click="clicked"
+      >
+        CLICK ME
       </div>
     </div>
   </div>
@@ -25,7 +34,14 @@ export default {
     return {
       continents: store.getters['continents/continents'],
       tripIdeas: store.getters['tripIdeas/tripIdeas'],
+      counter: 0,
     }
+  },
+  methods: {
+    clicked() {
+      this.counter++
+      console.log(this.counter)
+    },
   },
 }
 </script>
