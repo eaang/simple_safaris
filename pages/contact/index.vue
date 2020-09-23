aa195354963a7ee37ce3a6416ec478a598e4dd1d
Evan Ang <alethiologist@gmail.com>
2020-09-23 16:39:35 +0800

    tweaking contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,32 +1,50 @@
 <template>
-  <!-- Hero section -->
-  <div
-    class="bg-cover bg-center w-full h-screen flex flex-col items-center justify-end"
-    style="background-image: url('/migration.jpg')"
-  >
-    <div class="flex-grow flex-center pt-16">
-      <div class="title-header">Contact us</div>
-    </div>
-    <div class="w-5/6 sm:w-3/4 bg-white h-screen/25">
-      <div class="relative w-full h-full flex flex-col justify-center">
-        <div
-          class="flex flex-wrap justify-center gap-x-2 text-2xl sm:text-3xl xl:text-4xl italic p-4"
-        >
-          <div class="text-center">“아침에 일어날때 한번도 불쾌한</div>
-          <div class="text-center">마음이 들지 않았던 그곳...</div>
-          <div class="text-center">그곳이 바로 아프리카였다.”</div>
+  <div class="contact-page">
+    <!-- Hero section -->
+    <div
+      class="bg-cover bg-center w-full h-screen flex flex-col items-center justify-end"
+      style="background-image: url('/migration.jpg')"
+    >
+      <div class="flex-grow flex-center pt-16">
+        <div class="title-header">Contact us</div>
+      </div>
+      <div class="w-5/6 sm:w-3/4 bg-white h-screen/25">
+        <div class="relative w-full h-full flex flex-col justify-center">
+          <div
+            class="flex flex-wrap justify-center gap-x-2 text-2xl sm:text-3xl xl:text-4xl italic p-4"
+          >
+            <div class="text-center">“아침에 일어날때 한번도 불쾌한</div>
+            <div class="text-center">마음이 들지 않았던 그곳...</div>
+            <div class="text-center">그곳이 바로 아프리카였다.”</div>
+          </div>
+          <div
+            class="hidden lg:block absolute inset-x-0 bottom-0 text-brown text-2xl text-center"
+          >
+            소설가 Ernest Hemingway
+          </div>
         </div>
-        <div
-          class="hidden lg:block absolute inset-x-0 bottom-0 text-brown text-2xl text-center"
-        >
+      </div>
+      <div class="w-full flex flex-col justify-between bg-white h-screen/25">
+        <div class="block lg:hidden text-brown text-xl sm:text-2xl text-center">
           소설가 Ernest Hemingway
         </div>
+        <div class="w-full flex justify-stretch flex-center py-16">
+          <div class="w-24 h-1 bg-brown"></div>
+        </div>
+        <div class="text-black text-center text-lg md:text-xl px-8 w-full">
+          <div>
+            심플사파리에서 당신을 위한 100% 맞춤 사파리 여행을 계획해 드립니다.
+          </div>
+          <div>아래 사항들을 기재해 심플사파리에게 보내주세요.</div>
+        </div>
       </div>
     </div>
-    <div class="w-full bg-white h-screen/25">
-      <div class="block lg:hidden text-brown text-xl sm:text-2xl text-center">
-        소설가 Ernest Hemingway
-      </div>
+
+    <!-- Contact form here -->
+    <div class="contact-form container mx-auto pt-8">
+      <div
+        class="bg-gray-lighter border-t-4 border-b-4 border-brown h-screen"
+      ></div>
     </div>
   </div>
 </template>
