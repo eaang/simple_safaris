7aec3cf3a82278f55b90a759b691b7108d72b985
Evan Ang <alethiologist@gmail.com>
2020-09-30 18:11:09 +0800

    moved code to asyncdata instead

--- pages/trips/_slug/index.vue
+++ pages/trips/_slug/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="trip-idea-page">
     <!-- Hero Section -->
-
     <div class="relative h-screen/75 w-full bg-blue-700 flex flex-center">
       <div class="mt-16 title-header">{{ tripName }}</div>
     </div>
-    {{ tripIdea.fields.name }}
-    {{ tripDescription }}
+
+    <!-- Main content -->
+    <div>{{ tripDescription }}</div>
   </div>
 </template>
 
@@ -15,15 +15,21 @@ export default {
   async fetch({ store, params }) {
     await store.dispatch('tripIdea/getTripIdeaBySlug', params.slug)
   },
-  asyncData({ store }) {
-    return { tripIdea: store.getters['tripIdea/tripIdea'] }
+  async asyncData({ store, params }) {
+    await store.dispatch('tripIdea/getTripIdeaBySlug', params.slug)
+    const thisTrip = store.getters['tripIdea/tripIdea']
+    return {
+      tripIdea: thisTrip,
+      tripName: thisTrip.fields.name,
+      tripDescription: thisTrip.fields.description.content[0].content[0].value,
+    }
   },
   computed: {
-    tripName() {
-      return this.tripIdea.fields.name
+    currentTrip() {
+      return this.$store.getters['tripIdea/tripIdea']
     },
-    tripDescription() {
-      return this.tripIdea.fields.description.content[0].content[0].value
+    isLoading() {
+      return this.$store.getters['tripIdea/loadingStatus']
     },
   },
 }
