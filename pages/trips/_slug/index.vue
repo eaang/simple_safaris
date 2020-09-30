bc3a3be0f66c21ca510e966ba54a49c826b50b03
Evan Ang <alethiologist@gmail.com>
2020-09-30 21:33:07 +0800

    yay the header works

--- pages/trips/_slug/index.vue
+++ pages/trips/_slug/index.vue
@@ -1,17 +1,82 @@
 <template>
   <div class="trip-idea-page">
     <!-- Hero Section -->
-    <div class="relative h-screen/75 w-full bg-blue-700 flex flex-center">
-      <div class="mt-16 title-header">{{ tripName }}</div>
+    <div class="relative h-screen/75 w-full">
+      <div class="absolute inset-x-0 top-0 z-0">
+        <agile
+          ref="carousel"
+          :options="carouselOptions"
+          @after-change="counter = $refs.carousel.getCurrentSlide()"
+        >
+          <div
+            v-for="image in tripPics"
+            :key="image.id"
+            class="slide h-screen/75 w-full pt-16"
+          >
+            <div
+              class="h-full w-full flex flex-center bg-cover bg-center"
+              :style="{
+                'background-image': 'url(' + image.fields.file.url + ')',
+              }"
+            >
+              <div class="title-header">{{ tripName }}</div>
+            </div>
+          </div>
+
+          <!-- Custom buttons -->
+          <template slot="prevButton"
+            ><div
+              class="absolute inset-y-0 left-0 flex items-center justify-around px-12 pt-16 text-2xl"
+            >
+              <div
+                class="rounded-full border-2 border-white p-6 text-white hover:bg-white hover:text-brown"
+              >
+                <ArrowLeft class="h-6 w-6" />
+              </div>
+            </div>
+          </template>
+          <template slot="nextButton"
+            ><div
+              class="absolute inset-y-0 right-0 flex items-center justify-around px-12 pt-16 text-2xl"
+            >
+              <div
+                class="rounded-full border-2 border-white p-6 text-white hover:bg-white hover:text-brown"
+              >
+                <ArrowRight class="h-6 w-6" />
+              </div>
+            </div>
+          </template>
+        </agile>
+        <!-- Custom dots -->
+        <ul class="flex justify-around absolute inset-x-0 bottom-0 py-12">
+          <div class="flex">
+            <li
+              v-for="(image, i) in tripPics"
+              :key="i"
+              class="border-2 border-white rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-white"
+              :class="{ 'bg-white': i === counter }"
+              @click="
+                counter = i
+                $refs.carousel.goTo(counter)
+              "
+            ></li>
+          </div>
+        </ul>
+      </div>
     </div>
 
-    <!-- Main content -->
-    <div>{{ tripDescription }}</div>
+    <!-- Map -->
+    <Map />
   </div>
 </template>
 
 <script>
+import { VueAgile } from 'vue-agile'
+
 export default {
+  components: {
+    agile: VueAgile,
+  },
   async fetch({ store, params }) {
     await store.dispatch('tripIdea/getTripIdeaBySlug', params.slug)
   },
@@ -21,16 +86,41 @@ export default {
     return {
       tripIdea: thisTrip,
       tripName: thisTrip.fields.name,
+      tripPics: thisTrip.fields.headerImages,
       tripDescription: thisTrip.fields.description.content[0].content[0].value,
     }
   },
+  data() {
+    return {
+      counter: 0,
+      carouselOptions: {
+        navButtons: false,
+        fade: true,
+        infinite: true,
+        autoplay: false,
+        dots: false,
+        speed: 4000,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              navButtons: true,
+            },
+          },
+        ],
+      },
+    }
+  },
   computed: {
-    currentTrip() {
-      return this.$store.getters['tripIdea/tripIdea']
-    },
     isLoading() {
       return this.$store.getters['tripIdea/loadingStatus']
     },
+  },
+  mounted() {
+    setInterval(() => {
+      this.$refs.carousel.goToNext()
+      this.counter++
+    }, 8000)
   },
 }
 </script>
