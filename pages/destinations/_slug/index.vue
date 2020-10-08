5aecea6ef58e831152d46d916a2568e4793dac7b
Evan Ang <alethiologist@gmail.com>
2020-10-09 02:31:39 +0800

    added CTA

--- pages/destinations/_slug/index.vue
+++ pages/destinations/_slug/index.vue
@@ -156,7 +156,7 @@
 
           <!-- Hotels -->
           <div class="px-8 xl:px-0">
-            <BorderTitle text="Accomodation Options" />
+            <BorderTitle text="Where to Stay" />
             <div
               class="relative border-l border-r border-b border-brown flex flex-center"
             >
@@ -185,6 +185,23 @@
             </div>
           </div>
           <TripIdeaCarouselSmall :trip-ideas="destinationTrips" />
+        </div>
+
+        <!-- Design Your Safari -->
+        <div class="section container mx-auto text-center">
+          <div class="title title-main text-black mb-8">
+            Design your safari!
+          </div>
+          <div class="content mb-4 mx-4">
+            <div>꿈꾸시는 추억의 맞춤 사파리 여행을 떠나보세요 :)</div>
+          </div>
+          <a href="/contact">
+            <Button
+              text="여행 문의하기"
+              :arrow="true"
+              classes="btn-big btn-dark-brown cursor-pointer w-3/5 md:w-1/3 mx-auto"
+            />
+          </a>
         </div>
       </div>
 
