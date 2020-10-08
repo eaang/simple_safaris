5819db1d37ed7398c8b91b5a8a87a31d7a883e81
Evan Ang <alethiologist@gmail.com>
2020-10-09 00:00:08 +0800

    okay all info including trip idea shows up now

--- pages/destinations/_slug/index.vue
+++ pages/destinations/_slug/index.vue
@@ -4,11 +4,11 @@
     <StandardHero :pictures="destinationPics" :title="destinationName" />
 
     <!-- Information -->
-    <div class="container mx-auto flex">
+    <div class="container mx-auto flex text-black">
       <!-- Content -->
-      <div class="w-2/3">
+      <div class="w-full xl:w-2/3 space-y-24">
         <!-- Destination Introduction -->
-        <div id="introduction" class="py-16">
+        <div id="introduction">
           <div>Intro to {{ destinationName }}</div>
           <!-- Map -->
           <div class="w-full h-screen/50">
@@ -23,8 +23,8 @@
           <div>
             <div>하이라이트</div>
             <div
-              v-for="highlight in destination.fields.highlights"
-              :key="highlight"
+              v-for="(highlight, id) in destination.fields.highlights"
+              :key="id"
             >
               {{ highlight.fields.highlight }}
             </div>
@@ -45,10 +45,11 @@
           v-for="place in destinationPlaces"
           :id="kebabCase(place.fields.englishName) + '-intro'"
           :key="place.id"
-          class="py-16"
         >
           <!-- Title -->
-          <div>{{ place.fields.name }}</div>
+          <div>
+            {{ place.fields.name }}
+          </div>
           <!-- Image Carousel -->
           <div v-for="image in place.fields.images" :key="image.id">
             <img :src="image.fields.file.url" :alt="image.fields.title" />
@@ -56,7 +57,7 @@
           <!-- Highlights -->
           <div>
             <div>하이라이트</div>
-            <div v-for="highlight in place.fields.highlights" :key="highlight">
+            <div v-for="(highlight, i) in place.fields.highlights" :key="i">
               {{ highlight.fields.highlight }}
             </div>
           </div>
@@ -66,14 +67,34 @@
           <div>
             <div>여행적기: ({{ place.fields.bestTime }})</div>
           </div>
+          <div class="px-8 xl:px-0">
+            <BorderTitle :text="place.fields.name + '의 호텔'" />
+            <div
+              class="relative border-l border-r border-b border-brown flex flex-center"
+            >
+              <HotelCarousel :hotels="place.fields.hotels" />
+            </div>
+          </div>
         </div>
 
-        <!-- Hotels -->
-        <div
-          v-for="place in destinationPlaces"
-          :key="place.id"
-          class="py-16"
-        ></div>
+        <!-- Trip Ideas -->
+        {{ destination.sys.id }}
+        <div v-if="destinationTrips.length > 0">
+          <div v-for="idea in destinationTrips" :key="idea.id">
+            <TripIdeaCard
+              :slug="idea.fields.slug"
+              :url="idea.fields.headerImages[0].fields.file.url"
+              :alt="idea.fields.headerImages[0].fields.file.title"
+              :title="idea.fields.name"
+              :content="
+                idea.fields.description.content[0].content[0].value.match(
+                  /^([^.]+.)/
+                )[0]
+              "
+              :price="idea.fields.startingPrice"
+            />
+          </div>
+        </div>
       </div>
       <!-- Navigation -->
       <div class="hidden xl:block xl:w-1/3 pl-16">
@@ -95,11 +116,7 @@
               ><div>{{ place.fields.name }}</div></nuxt-link
             >
           </div>
-          <div>Hotels</div>
-          <div v-for="place in destinationPlaces" :key="place.id">
-            {{ place.fields.name }}
-          </div>
-          <div>Trip Ideas</div>
+          <div v-if="destinationTrips.length > 0">Trip Ideas</div>
         </div>
       </div>
     </div>
@@ -107,25 +124,17 @@
 </template>
 
 <script>
+import client from '~/plugins/contentful.js'
+
 export default {
-  async asyncData({ store, params, $axios }) {
-    const space = process.env.CTF_SPACE_ID
-    const token = process.env.CTF_CD_ACCESS_TOKEN
+  async asyncData({ store, params }) {
     await store.dispatch('destination/getDestinationBySlug', params.slug)
     await store.dispatch('tripIdeas/getTripIdeas')
-    const destinationTrips = await $axios
-      .$get(
-        'https://cdn.contentful.com/spaces/' +
-          space +
-          '/entries?access_token=' +
-          token +
-          '&content_type=tripIdea&fields.destination.sys.contentType.sys.id=destination&fields.destination.fields.slug[match]=' +
-          params.slug
-      )
-      .then((res) => {
-        return res.items
-      })
     const destination = store.getters['destination/destination']
+    const getTrips = await client.getEntries({
+      content_type: 'tripIdea',
+      'fields.destination.sys.id': destination.sys.id
+    })
     const pics = destination.fields.headerImages
     const destinationPics = []
     pics.forEach((pic) => {
@@ -151,7 +160,7 @@ export default {
         parseFloat(destination.fields.mapCenter.fields.mapCentreLatitude)
       ],
       mapPoints: mapPoints,
-      destinationTrips
+      destinationTrips: getTrips.items
     }
   },
   data() {
