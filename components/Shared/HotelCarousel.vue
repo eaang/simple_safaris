6485ff2a1433c7ad8cf867cb2de112dcd165d9c3
Evan Ang <alethiologist@gmail.com>
2020-10-10 12:32:57 +0800

    modal success

--- components/Shared/HotelCarousel.vue
+++ components/Shared/HotelCarousel.vue
@@ -1,46 +1,69 @@
 <template>
   <div class="w-full">
-    <agile
+    <span
       v-if="hotels.length > 1"
-      :options="stayCarouselOptions"
-      class="w-full z-0 pointer-events-none"
+      v-touch:swipe.left="counterUp"
+      v-touch:swipe.right="counterDown"
     >
-      <div v-for="(hotel, index) in hotels" :key="index" class="slide">
-        <div class="w-full py-16 flex flex-center">
-          <HotelCard
-            :src="hotel.fields.hotelImage.fields.file.url"
-            :alt="hotel.fields.hotelImage.fields.title"
-            :name="hotel.fields.name"
-            :price="hotel.fields.price"
-            class="w-4/5 lg:w-2/5 pointer-events-auto"
-          />
+      <div class="relative mt-16 mb-32">
+        <div v-for="(hotel, index) in hotels" :key="index">
+          <transition name="fade" mode="out-in">
+            <div
+              v-show="index === trueCounter"
+              class="slide w-full h-full py-16 flex flex-center absolute"
+            >
+              <HotelCard
+                :src="hotel.fields.hotelImage.fields.file.url"
+                :alt="hotel.fields.hotelImage.fields.title"
+                :name="hotel.fields.name"
+                :price="hotel.fields.price"
+                class="w-4/5 lg:w-2/5"
+              />
+            </div>
+          </transition>
         </div>
-      </div>
-      <template slot="prevButton">
-        <div class="absolute inset-y-0 left-0 flex items-center">
+        <HotelCard class="invisible" />
+
+        <!-- Navigation Buttons -->
+        <div
+          class="invisible md:visible absolute inset-y-0 left-0 flex items-center cursor-pointer"
+        >
           <div
             class="w-16 h-24 border-t border-r border-b border-brown flex flex-center"
+            @click="counter--"
           >
             <AngleLeft class="text-brown h-16 pointer-events-auto" />
           </div>
         </div>
-      </template>
-      <template slot="nextButton">
-        <div class="absolute inset-y-0 right-0 flex items-center">
+        <div
+          class="invisible md:visible absolute inset-y-0 right-0 flex items-center cursor-pointer"
+        >
           <div
             class="w-16 h-24 border-t border-l border-b border-brown flex flex-center"
+            @click="counter++"
           >
             <AngleRight class="text-brown h-16 pointer-events-auto" />
           </div>
         </div>
-      </template>
-    </agile>
-
+        <!-- Custom dots -->
+        <ul class="flex justify-around absolute inset-x-0 bottom-0 -mb-16">
+          <div class="flex">
+            <li
+              v-for="(hotel, i) in hotels"
+              :key="i"
+              class="border-2 border-brown rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-brown"
+              :class="{ 'bg-brown': i === trueCounter }"
+              @click="counter = i"
+            ></li>
+          </div>
+        </ul>
+      </div>
+    </span>
     <div
       v-for="(hotel, index) in hotels"
       v-else
       :key="index"
-      class="w-full py-16 flex flex-center"
+      class="w-full h-full py-16 flex flex-center"
     >
       <HotelCard
         :src="hotel.fields.hotelImage.fields.file.url"
@@ -54,11 +77,7 @@
 </template>
 
 <script>
-import { VueAgile } from 'vue-agile'
 export default {
-  components: {
-    agile: VueAgile
-  },
   props: {
     hotels: {
       type: Array,
@@ -73,8 +92,8 @@ export default {
                 title: 'Placeholder Photo 1'
               }
             },
-            name: 'Hotel Name',
-            price: '2'
+            name: 'Hotel 1',
+            price: 2
           }
         },
         {
@@ -87,8 +106,36 @@ export default {
                 title: 'Placeholder Photo 2'
               }
             },
-            name: 'Name of Hotel',
-            price: '3'
+            name: 'Hotel 2',
+            price: 3
+          }
+        },
+        {
+          fields: {
+            hotelImage: {
+              fields: {
+                file: {
+                  url: 'https://picsum.photos/600/400?random=3'
+                },
+                title: 'Placeholder Photo 3'
+              }
+            },
+            name: 'Hotel 3',
+            price: 3
+          }
+        },
+        {
+          fields: {
+            hotelImage: {
+              fields: {
+                file: {
+                  url: 'https://picsum.photos/600/400?random=4'
+                },
+                title: 'Placeholder Photo 4'
+              }
+            },
+            name: 'Hotel 4',
+            price: 3
           }
         }
       ]
@@ -96,22 +143,30 @@ export default {
   },
   data() {
     return {
-      stayCarouselOptions: {
-        navButtons: false,
-        fade: false,
-        infinite: true,
-        autoplay: false,
-        dots: false,
-        speed: 2000,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              navButtons: true
-            }
-          }
-        ]
+      counter: 0
+    }
+  },
+  computed: {
+    trueCounter() {
+      if (this.counter < 0) {
+        if ((this.counter * -1) % this.hotels.length === 0) {
+          return 0
+        } else {
+          return this.hotels.length - ((this.counter * -1) % this.hotels.length)
+        }
+      } else if (this.counter < this.hotels.length) {
+        return this.counter
+      } else {
+        return this.counter % this.hotels.length
       }
+    }
+  },
+  methods: {
+    counterUp() {
+      this.counter++
+    },
+    counterDown() {
+      this.counter--
     }
   }
 }
--- pages/testing.vue
+++ pages/testing.vue
@@ -3,78 +3,80 @@
   <div class="w-full">
     <div class="mt-16"></div>
     <!-- Card Carousel -->
-    <span
-      v-if="hotels.length > 1"
-      v-touch:swipe.left="counterUp"
-      v-touch:swipe.right="counterDown"
-    >
-      <div class="relative">
-        <div v-for="(hotel, index) in hotels" :key="index">
-          <transition name="fade" mode="out-in">
-            <div
-              v-show="index === trueCounter"
-              class="slide w-full h-full py-16 flex flex-center absolute"
-            >
-              <HotelCard
-                :src="hotel.fields.hotelImage.fields.file.url"
-                :alt="hotel.fields.hotelImage.fields.title"
-                :name="hotel.fields.name"
-                :price="hotel.fields.price"
-                class="w-4/5 lg:w-2/5"
-              />
-            </div>
-          </transition>
-        </div>
-        <HotelCard class="invisible" />
+    <div>
+      <span
+        v-if="hotels.length > 1"
+        v-touch:swipe.left="counterUp"
+        v-touch:swipe.right="counterDown"
+      >
+        <div class="relative">
+          <div v-for="(hotel, index) in hotels" :key="index">
+            <transition name="fade" mode="out-in">
+              <div
+                v-show="index === trueCounter"
+                class="slide w-full h-full py-16 flex flex-center absolute"
+              >
+                <HotelCard
+                  :src="hotel.fields.hotelImage.fields.file.url"
+                  :alt="hotel.fields.hotelImage.fields.title"
+                  :name="hotel.fields.name"
+                  :price="hotel.fields.price"
+                  class="w-4/5 lg:w-2/5"
+                />
+              </div>
+            </transition>
+          </div>
+          <HotelCard class="invisible" />
 
-        <!-- Navigation Buttons -->
-        <div
-          class="invisible md:visible absolute inset-y-0 left-0 flex items-center cursor-pointer"
-        >
+          <!-- Navigation Buttons -->
           <div
-            class="w-16 h-24 border-t border-r border-b border-brown flex flex-center"
-            @click="counter--"
+            class="invisible md:visible absolute inset-y-0 left-0 flex items-center cursor-pointer"
           >
-            <AngleLeft class="text-brown h-16 pointer-events-auto" />
+            <div
+              class="w-16 h-24 border-t border-r border-b border-brown flex flex-center"
+              @click="counter--"
+            >
+              <AngleLeft class="text-brown h-16 pointer-events-auto" />
+            </div>
           </div>
-        </div>
-        <div
-          class="invisible md:visible absolute inset-y-0 right-0 flex items-center cursor-pointer"
-        >
           <div
-            class="w-16 h-24 border-t border-l border-b border-brown flex flex-center"
-            @click="counter++"
+            class="invisible md:visible absolute inset-y-0 right-0 flex items-center cursor-pointer"
           >
-            <AngleRight class="text-brown h-16 pointer-events-auto" />
+            <div
+              class="w-16 h-24 border-t border-l border-b border-brown flex flex-center"
+              @click="counter++"
+            >
+              <AngleRight class="text-brown h-16 pointer-events-auto" />
+            </div>
           </div>
+          <!-- Custom dots -->
+          <ul class="flex justify-around absolute inset-x-0 bottom-0 -mb-16">
+            <div class="flex">
+              <li
+                v-for="(hotel, i) in hotels"
+                :key="i"
+                class="border-2 border-brown rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-brown"
+                :class="{ 'bg-brown': i === trueCounter }"
+                @click="counter = i"
+              ></li>
+            </div>
+          </ul>
         </div>
-        <!-- Custom dots -->
-        <ul class="flex justify-around absolute inset-x-0 bottom-0 -mb-16">
-          <div class="flex">
-            <li
-              v-for="(hotel, i) in hotels"
-              :key="i"
-              class="border-2 border-brown rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-brown"
-              :class="{ 'bg-brown': i === trueCounter }"
-              @click="counter = i"
-            ></li>
-          </div>
-        </ul>
+      </span>
+      <div
+        v-for="(hotel, index) in hotels"
+        v-else
+        :key="index"
+        class="w-full h-full py-16 flex flex-center"
+      >
+        <HotelCard
+          :src="hotel.fields.hotelImage.fields.file.url"
+          :alt="hotel.fields.hotelImage.fields.title"
+          :name="hotel.fields.name"
+          :price="hotel.fields.price"
+          class="w-4/5 lg:w-2/5"
+        />
       </div>
-    </span>
-    <div
-      v-for="(hotel, index) in hotels"
-      v-else
-      :key="index"
-      class="w-full h-full py-16 flex flex-center"
-    >
-      <HotelCard
-        :src="hotel.fields.hotelImage.fields.file.url"
-        :alt="hotel.fields.hotelImage.fields.title"
-        :name="hotel.fields.name"
-        :price="hotel.fields.price"
-        class="w-4/5 lg:w-2/5"
-      />
     </div>
   </div>
 </template>
