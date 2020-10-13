f5d1988508cb983cf9968bee140a1c145a06aaca
Evan Ang <alethiologist@gmail.com>
2020-10-13 21:15:45 +0800

    fixed map on about page not showing up

---
+++ pages/testing.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="h-16"></div>
+    <Map
+      class="w-full h-screen/50 lg:h-screen/75"
+      :center="center"
+      :places="places"
+      :zoom="zoom"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      center: [0, 0],
+      places: [
+        { name: 'Kenya', longitude: 37.906193, latitude: -0.023559 },
+        { name: 'Tanzania', longitude: 34.888821, latitude: -6.369028 },
+        { name: 'Botswana', longitude: 24.684866, latitude: -22.328474 },
+        { name: 'Namibia', longitude: 18.49041, latitude: -22.95764 },
+        { name: 'South Africa', longitude: 22.937506, latitude: -30.559483 }
+      ],
+      zoom: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
