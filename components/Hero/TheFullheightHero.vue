ddb06e7ce7cdfa52672588052b5b3a9c137dc480
Evan Ang <alethiologist@gmail.com>
2020-10-18 20:24:22 +0800

    trying to client only the main carousel

--- components/Hero/TheFullheightHero.vue
+++ components/Hero/TheFullheightHero.vue
@@ -1,58 +1,60 @@
 <template>
   <div>
     <div class="relative h-screen/75 lg:h-screen w-full">
-      <agile
-        ref="carousel"
-        :options="carouselOptions"
-        @after-change="counter = $refs.carousel.getCurrentSlide()"
-      >
-        <div
-          v-for="(image, index) in images"
-          :key="index"
-          :class="image.position"
-          class="slide block h-screen/75 lg:h-screen lg:bg-bottom w-full z-0 bg-cover"
-          :style="{
-            backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0)), url(${image.src})`
-          }" />
+      <client-only>
+        <agile
+          ref="carousel"
+          :options="carouselOptions"
+          @after-change="counter = $refs.carousel.getCurrentSlide()"
+        >
+          <div
+            v-for="(image, index) in images"
+            :key="index"
+            :class="image.position"
+            class="slide block h-screen/75 lg:h-screen lg:bg-bottom w-full z-0 bg-cover"
+            :style="{
+              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0)), url(${image.src})`
+            }" />
 
-        <!-- Custom buttons -->
-        <template slot="prevButton"
-          ><div
-            class="absolute inset-y-0 left-0 flex items-center justify-around px-12 text-2xl"
-          >
-            <div
-              class="rounded-full border-2 border-white p-6 text-white hover:bg-white hover:text-brown"
+          <!-- Custom buttons -->
+          <template slot="prevButton"
+            ><div
+              class="absolute inset-y-0 left-0 flex items-center justify-around px-12 text-2xl"
             >
-              <ArrowLeft class="h-6 w-6" />
+              <div
+                class="rounded-full border-2 border-white p-6 text-white hover:bg-white hover:text-brown"
+              >
+                <ArrowLeft class="h-6 w-6" />
+              </div>
             </div>
+          </template>
+          <template slot="nextButton"
+            ><div
+              class="absolute inset-y-0 right-0 flex items-center justify-around px-12 text-2xl"
+            >
+              <div
+                class="rounded-full border-2 border-white p-6 text-white hover:bg-white hover:text-brown"
+              >
+                <ArrowRight class="h-6 w-6" />
+              </div>
+            </div> </template
+        ></agile>
+        <!-- Custom dots -->
+        <ul class="flex justify-around absolute inset-x-0 bottom-0 py-12">
+          <div class="flex">
+            <li
+              v-for="(image, i) in images"
+              :key="i"
+              class="border-2 border-white rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-white"
+              :class="{ 'bg-white': i === counter }"
+              @click="
+                counter = i
+                $refs.carousel.goTo(counter)
+              "
+            ></li>
           </div>
-        </template>
-        <template slot="nextButton"
-          ><div
-            class="absolute inset-y-0 right-0 flex items-center justify-around px-12 text-2xl"
-          >
-            <div
-              class="rounded-full border-2 border-white p-6 text-white hover:bg-white hover:text-brown"
-            >
-              <ArrowRight class="h-6 w-6" />
-            </div>
-          </div> </template
-      ></agile>
-      <!-- Custom dots -->
-      <ul class="flex justify-around absolute inset-x-0 bottom-0 py-12">
-        <div class="flex">
-          <li
-            v-for="(image, i) in images"
-            :key="i"
-            class="border-2 border-white rounded-full w-4 h-4 mx-2 cursor-pointer hover:bg-white"
-            :class="{ 'bg-white': i === counter }"
-            @click="
-              counter = i
-              $refs.carousel.goTo(counter)
-            "
-          ></li>
-        </div>
-      </ul>
+        </ul>
+      </client-only>
     </div>
   </div>
 </template>
